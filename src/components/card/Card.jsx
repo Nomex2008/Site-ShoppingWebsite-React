317a9153b13d9a9c4d529b92026442f4cbc3af05
Alex <97978327+Nomex2008@users.noreply.github.com>
2024-02-19 00:30:31 +0200

    Commit 7 (card2)
    Add more content to component card.

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,34 @@
 import styles from './Card.module.css'
+import car01Img from './../../img/categories/cat-01.jpg'
+import arrowImg from './../../img/icons/arrow.svg'
+
 
 const Card = () => {
     return ( 
         <div className={styles.card}>
-            Card
+
+            <img className={styles.card__img} src={car01Img} alt="" />
+            
+            <div className={styles.card__body}>
+
+                <div className={styles.card__text}>
+
+                    <div className={styles.card__title}>
+                        Hoodies & Sweetshirt
+                    </div>
+
+                    <div className={styles.card__muted}>
+                        Explore Now!
+                    </div>
+                    
+                </div>
+
+                <div className={styles.card__icon}>
+                    <img src={arrowImg} alt="arrowImg" />
+                </div>
+
+            </div>
+
         </div>
      );
 }
