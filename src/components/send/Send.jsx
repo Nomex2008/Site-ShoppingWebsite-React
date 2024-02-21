8113c71e87d445e998e50db5a4b8c7aa414a8e9c
Alex <97978327+Nomex2008@users.noreply.github.com>
2024-02-21 20:41:43 +0200

    Commit 13 (send2)
    Add css for component send.

--- src/components/send/Send.jsx
+++ src/components/send/Send.jsx
@@ -5,19 +5,19 @@ const Send = () => {
         <div className={style.send}>
             <div className={style.send__container}>
                 
-                <h3 className="send__title">
+                <h3 className={style.send__title}>
                     JOIN SHOPPING COMMUNITY TO GET MONTHLY PROMO
                 </h3>
 
-                <div className="send__subtitle">
+                <div className={style.send__subtitle}>
                     <p>
                         Type your email down below and be young wild generation
                     </p>
                 </div>
 
-                <form action="" className="send__form">
-                    <input type="email" placeholder='Add your email here' className="send__input" />
-                    <button className="send__btn">
+                <form action="" className={style.send__form}>
+                    <input type="email" placeholder='Add your email here' className={style.send__input} />
+                    <button className={style.send__btn}>
                         SEND
                     </button>
                 </form>
